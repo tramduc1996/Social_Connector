7db3a545deaced35d65d57eea4a69541e8d7327c
Duc Tram <tramduc1996@yahoo.com>
2018-12-09 02:57:55 -0800

    Axios for register and login

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,28 +1,69 @@
-import React from 'react'
+import React from "react";
+import axios from "axios"
 
- class Login extends React.Component {
+class Login extends React.Component {
+  state = {
+    email: "",
+    password: "",
+
+    errors: {}
+  };
+
+  onChange = e => {
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState({[name]: value})
+  }
+  onSubmit = e => {
+    e.preventDefault();
+    const {email, password} = this.state;
+    const newUser = {
+   email, password
+    }
+     axios
+    .post("/api/users/login", newUser)
+    .then(res => console.log(res.data))
+    .catch(err => console.log(err.response.data));
+      }
+
   render() {
     return (
-      <div class="login">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-8 m-auto">
-            <h1 class="display-4 text-center">Log In</h1>
-            <p class="lead text-center">Sign in to your DevConnector account</p>
-            <form action="dashboard.html">
-              <div class="form-group">
-                <input type="email" class="form-control form-control-lg" placeholder="Email Address" name="email" />
-              </div>
-              <div class="form-group">
-                <input type="password" class="form-control form-control-lg" placeholder="Password" name="password" />
-              </div>
-              <input type="submit" class="btn btn-info btn-block mt-4" />
-            </form>
+      <div className="login">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Log In</h1>
+              <p className="lead text-center">
+                Sign in to your DevConnector account
+              </p>
+              <form onSubmit={this.onSubmit}>
+                <div className="form-group">
+                  <input
+                    type="email"
+                    className="form-control form-control-lg"
+                    placeholder="Email Address"
+                    name="email"
+                    value={this.state.email}
+                    onChange={this.onChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="password"
+                    className="form-control form-control-lg"
+                    placeholder="Password"
+                    name="password"
+                    value={this.state.password}
+                    onChange={this.onChange}
+                  />
+                </div>
+                <input type="submit" className="btn btn-info btn-block mt-4" />
+              </form>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    )
+    );
   }
 }
-export default Login
+export default Login;
